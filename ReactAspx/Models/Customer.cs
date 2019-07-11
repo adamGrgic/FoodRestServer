b67e0f9c40f0a2ad1645db870adced2468259d1a
Adam <argrgic@gmail.com>
2019-07-11 09:01:02 -0500

    Added new connection string and AppDbContext.

--- ReactAspx/Models/Customer.cs
+++ ReactAspx/Models/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,12 @@ namespace ReactAspx.Models
 {
     public class Customer
     {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        [NotMapped]
+        public string ConfirmPassword { get; set; }
     }
 }
