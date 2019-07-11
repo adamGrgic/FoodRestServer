68ccb5796971b84fd65b3e0df3508e9f8968645a
Adam <argrgic@gmail.com>
2019-07-11 14:34:48 -0500

    added menu display items through rendering.

---
+++ ReactAspx/Content/src/MenuBox.tsx
@@ -0,0 +1,36 @@
+﻿import * as React from "react";
+
+import * as ReactDOM from "react-dom";
+
+import { FoodModel, IAppState } from "./Models";
+
+export class MenuBox extends React.Component<any, IAppState> {
+    constructor(state) {
+        super(state);
+        this.state = { items: null, myOrder: null, showPopup: false, userId: 0, orderPlaced: false };
+        this.loadMenusFromServer();
+    }
+
+    loadMenusFromServer() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('get', '/data/GetMenuList/', true);
+        xhr.onload = function () {
+            var dataitems = JSON.parse(xhr.responseText);
+            var tmp: IAppState = this.state;
+            tmp.items = dataitems;
+            this.setState(tmp);
+        }.bind(this);
+        xhr.send();
+
+    }
+
+    render() {
+        let menus = this.state.items || [];
+        var menuList = menus.map(function (menu)) {
+            return (
+                <div>testabc</div>);
+        }
+        
+    }
+   
+}
